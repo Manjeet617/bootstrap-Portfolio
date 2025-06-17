08c0c7c7ff40fabf439cbd9d3fc467b3de560ec0
Manjeet617 <manjeetbajiya48@gmail.com>
2025-06-17 17:31:20 +0530

    u

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,47 +2,46 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    const {toggle,setToggle} = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    const handleToggle = () =>{
-        setToggle(!toggle);
-    };
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-    
   return (
     <nav className="navbar navbar-expand-lg bg-secondary navbar-light fixed-top">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Portfolio</Link>
+        <Link className="navbar-brand text-white" to="/">Manjeet</Link>
 
         <button
           className="navbar-toggler"
           type="button"
+          onClick={handleToggle}
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded={toggle ? "true" : "false"}
           aria-label="Toggle navigation"
-          onClick={handleToggle}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${toggle ? "show" : ""}`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link " onClick={handleToggle}>Home</Link>
+              <Link to="/" className="nav-link text-white" onClick={handleToggle}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/about" className="nav-link" onClick={handleToggle}>About</Link>
+              <Link to="/about" className="nav-link text-white" onClick={handleToggle}>About</Link>
             </li>
             <li className="nav-item">
-              <Link to="/skills" className="nav-link"onClick={handleToggle}>Skills</Link>
+              <Link to="/skills" className="nav-link text-white" onClick={handleToggle}>Skills</Link>
             </li>
             <li className="nav-item">
-              <Link to="/projects" className="nav-link"onClick={handleToggle}>Projects</Link>
+              <Link to="/projects" className="nav-link text-white" onClick={handleToggle}>Projects</Link>
             </li>
             <li className="nav-item">
-              <Link to="/contact" className="nav-link"onClick={handleToggle}>Contact</Link>
+              <Link to="/contact" className="nav-link text-white" onClick={handleToggle}>Contact</Link>
             </li>
           </ul>
         </div>
